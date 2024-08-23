aa64ca0e744765b5c22ec8d87a7e3b3c5f90a78e
gory3456 <fantan1888@gmail.com>
2024-08-23 18:37:05 +0300

    модалка+меню js1

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,36 @@
+const modal = document.querySelector('.slick-notification');
+const modalClosed = document.querySelector('.slick-notification__close')
+const modalContinue = document.querySelector('.slick-notification__accept');
+const modalOpen = document.querySelectorAll('.product__button');
 
+modalOpen.forEach((button) => {
+    button.addEventListener('click', () => {
+        modal.classList.add('slick-notification--showed');
+        modalClosed.addEventListener('click', closeModal);
+        modalContinue.addEventListener('click', closeModal);
+    })
+})
+
+const closeModal = () => {
+    modal.classList.remove('slick-notification--showed');
+    modalClosed.remdoveEventListener('click', closeModal);
+    modalContinue.removeEventListener('click', closeModal);
+}
+
+
+const buttonBurger = document.querySelector('.main-nav__button--burger');
+const mainNav = document.querySelector('.main-nav__menu');
+const mainNavButtons = document.querySelector('.main-nav__buttons');
+const buttonClosed = document.querySelector('.main-nav__button--close');
+
+const closeMainNav = () => {
+    mainNav.classList.remove('main-nav__menu--showed');
+    mainNavButtons.classList.remove('main-nav__buttons--showed');
+    buttonClosed.removeEventListener('click', closeMainNav);
+};
+
+buttonBurger.addEventListener('click', () => {
+    mainNav.classList.add('main-nav__menu--showed');
+    mainNavButtons.classList.add('main-nav__buttons--showed');
+    buttonClosed.addEventListener('click', closeMainNav);
+});
