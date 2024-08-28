68c4ebdcd802cdc4af7438f50db45b7047eb86fc
gory3456 <fantan1888@gmail.com>
2024-08-28 11:02:42 +0300

    доделал кнопки меню

--- source/js/script.js
+++ source/js/script.js
@@ -18,7 +18,7 @@ const closeModal = () => {
 }
 
 
-const buttonBurger = document.querySelector('.main-nav__button--burger');
+const buttonBurger = document.querySelector('.burger-button--showed');
 const mainNav = document.querySelector('.main-nav__menu');
 const mainNavButtons = document.querySelector('.main-nav__buttons');
 const buttonClosed = document.querySelector('.main-nav__button--close');
@@ -26,11 +26,14 @@ const buttonClosed = document.querySelector('.main-nav__button--close');
 const closeMainNav = () => {
     mainNav.classList.remove('main-nav__menu--showed');
     mainNavButtons.classList.remove('main-nav__buttons--showed');
+	buttonBurger.classList.add('burger-button--showed');
     buttonClosed.removeEventListener('click', closeMainNav);
 };
 
 buttonBurger.addEventListener('click', () => {
     mainNav.classList.add('main-nav__menu--showed');
     mainNavButtons.classList.add('main-nav__buttons--showed');
+	buttonBurger.classList.remove('burger-button--showed');
+
     buttonClosed.addEventListener('click', closeMainNav);
 });
